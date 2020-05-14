a3ed594647e11a5a94ed74def8adb1b2901ad9a6
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2020-05-14 13:45:37 -0300

    Show comments

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -7,9 +7,14 @@ const Publication = require('../models/Publication')
 module.exports = {
   index: async (req, res) => {
     let publications = await Publication.findAll({
-      include: {
+      include: [{
         association: 'user'
-      }
+      },{
+        association: 'comments',
+        include: {
+          association: 'user'
+        }
+      }]
     })
 
     res.render('index', { publications, moment })
@@ -19,12 +24,20 @@ module.exports = {
 
     const userPublications = await User.findByPk(user.id, {
       include: {
-        association: 'publications'
+        association: 'publications',
+        include: [{
+          association: 'user'
+        },{
+          association: 'comments',
+          include: {
+            association: 'user'
+          }
+        }]
       }
     })
 
     const publications = userPublications.publications
 
-    res.render('my_posts', { publications })
+    res.render('my_posts', { publications, moment })
   }
 }
